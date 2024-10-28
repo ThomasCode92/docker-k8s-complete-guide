932c90c271870ea1166c0b19ae53ac05bfec34c7
Thomas <thomascode92@hotmail.com>
2024-10-28 20:04:43 +0100

    App Server Starter Code

---
+++ apps/visits/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import redis from 'redis';
+
+const app = express();
+const client = redis.createClient();
+
+client.set('visits', 0);
+
+app.get('/', async function (req, res) {
+  const visits = await client.get('visits');
+  await client.set('visits', parseInt(visits) + 1);
+
+  return res.send('Number of visits: ' + visits);
+});
+
+app.listen(8080, function () {
+  console.log('Listening on port 8080');
+});
