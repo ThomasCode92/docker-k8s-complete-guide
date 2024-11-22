15d42f56598e846d8f2d2cb914be73c1290ebaab
Thomas <thomascode92@hotmail.com>
2024-11-22 08:24:02 +0100

    More Express API Setup

--- apps/fibonacci/server/index.js
+++ apps/fibonacci/server/index.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import pg from 'pg';
+import redis from 'redis';
 
 import keys from './config/keys.js';
 
@@ -25,4 +26,45 @@ pgClient.on('connect', client => {
   client
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.error(err));
+});
+
+// Redis Client Setup
+const redisClient = redis.createClient({
+  url: `redis://${keys.redisHost}:${keys.redisPort}`,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get('/', function (_req, res) {
+  return res.json({ message: 'server is running' });
+});
+
+app.get('/values/all', async function (_req, res) {
+  const values = await pgClient.query('SELECT * from values');
+  return res.json({ values: values.rows });
+});
+
+app.get('/values/current', async function (_req, res) {
+  const values = await redisClient.hGetAll('values');
+  return res.json(values);
+});
+
+app.post('/values', async function (req, res) {
+  const { index } = req.body;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).json({ error: 'Index too high' });
+  }
+
+  redisClient.hSet('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  return res.status(201).json({ message: 'Index inserted successfully' });
+});
+
+app.listen(5000, function () {
+  console.log('Server listening on port 5000...');
 });
