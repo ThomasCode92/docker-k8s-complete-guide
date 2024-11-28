33757ee01d609ab8ec1ebc4e73ba351dfb00e400
Thomas <thomascode92@hotmail.com>
2024-11-28 19:37:25 +0100

    Fixing the Unit Tests

--- apps/fibonacci/client/src/App.test.jsx
+++ apps/fibonacci/client/src/App.test.jsx
@@ -1,6 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+vi.mock('./Fibonacci', function () {
+  return {
+    default: function () {
+      return <div>Fib Component</div>;
+    },
+  };
+});
+
 test('should render the application title', function () {
   render(<App />);
   const titleElement = screen.getByRole('heading', { name: /fibonacci/i });
