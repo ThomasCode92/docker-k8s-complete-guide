1030ecc6d32c3fec49f562d6b11fb5c8ae6c1c3c
Thomas <thomascode92@hotmail.com>
2025-03-28 08:58:09 +0100

    Adding Support for 'mdx'

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
-
+import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  integrations: [mdx()],
 });
+
